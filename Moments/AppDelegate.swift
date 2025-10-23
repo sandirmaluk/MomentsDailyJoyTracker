d1517fa3660dd4a0208913f0bb42abfb26d53a10
sandirmaluk <sandirmaluk@icloud.com>
2025-10-23 11:35:41 +0500

    Delete Moments directory

--- Moments/AppDelegate.swift
+++
@@ -1,9 +0,0 @@
-import UIKit
-
-class AppDelegate: NSObject, UIApplicationDelegate {
-    static var orientationLock = UIInterfaceOrientationMask.portrait
-    
-    func application(_ application: UIApplication, supportedInterfaceOrientationsFor window: UIWindow?) -> UIInterfaceOrientationMask {
-        return AppDelegate.orientationLock
-    }
-}
